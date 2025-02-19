1472814f7e69c86abe89c452e0444a97206845b7
tangcxx <58771897+tangcxx@users.noreply.github.com>
2025-02-19 17:14:41 +0800

    新增神经网络作弊机器人和训练模块，优化模型创建与数据处理逻辑

--- bot_aug.py
+++ bot_aug.py
@@ -6,11 +6,11 @@ import rules
 to_categorical = k.utils.to_categorical
 
 NCARDGROUPS = 3  ##神经网络输入牌组数量
-CARD_DIM = rules.CARD_DIM  ## 牌组长度, 15
-NCHANNEL = 9
+CARD_DIM = rules.CARD_DIM  ## 牌组长度，15
+NCHANNEL = 16
 
-##使用神经网络出牌的作弊机器人, 它知道其他玩家手上的牌
-## BOT类是演示用的, 供ARENA类调用
+##使用神经网络出牌的作弊机器人，它知道其他玩家手上的牌
+## BOT类是演示用的，供ARENA类调用
 ## ARENA类要求所有BOT有如下4个成员函数
 class BOT:
     def __init__(self, model, verbos=0, epsilon=0):
@@ -27,19 +27,19 @@ class BOT:
         x = k.layers.Conv2D(512, (1, CARD_DIM))(inputs)
         x = k.layers.BatchNormalization()(x)
         x = k.layers.ReLU()(x)
-        x = k.layers.Dropout(0.5)(x)
+        x = k.layers.Dropout(0.2)(x)
 
         x = k.layers.Conv2D(512, (NCARDGROUPS, 1))(x)
         x = k.layers.BatchNormalization()(x)
         x = k.layers.ReLU()(x)
-        x = k.layers.Dropout(0.5)(x)
+        x = k.layers.Dropout(0.2)(x)
 
         x = k.layers.Flatten()(x)
 
         x = k.layers.Dense(256)(x)
         x = k.layers.BatchNormalization()(x)
         x = k.layers.ReLU()(x)
-        x = k.layers.Dropout(0.5)(x)
+        x = k.layers.Dropout(0.2)(x)
 
         outputs = k.layers.Dense(1, activation="sigmoid")(x)
         model = k.models.Model(inputs, outputs)
@@ -65,16 +65,55 @@ class BOT:
     def update(self):
         pass
 
+    # 0,1,2,3层: 手牌
+    # 4,5,6,7: 上一轮出牌
+    # 8: 当前出牌人
+    # 9: 单张
+    # 10: 对子
+    # 11: 三张
+    # 12：炸弹
+    # 13：顺子
+    # 14: 连对
+    # 15: 飞机
     @classmethod
     def getdata(cls, arena):
         data = np.zeros((NCARDGROUPS, CARD_DIM, NCHANNEL), int)
+        remain = arena.remain
+        lastplay = arena.lastplay
         
+        data[:, :, 0] = remain > 0                      ## 0
+        data[:, :, 1] = remain > 1                      ## 1
+        data[:, :, 2] = remain > 2                     ## 2
+        data[:, :, 3] = remain > 3                     ## 3
+        
+        data[:, :, 4] = lastplay > 0                     ## 4
+        data[:, :, 5] = lastplay > 1                     ## 5
+        data[:, :, 6] = lastplay > 2                     ## 6
+        data[:, :, 7] = lastplay > 3                     ## 7
+        
+        data[arena.pos, :, 4:8] = 0                        ## 出牌人的上一轮出牌不考虑
+
+        data[arena.pos, :, 8] = 1                        ## 8
+        
+        data[:, :, 9] = remain == 1                      ## 9
+        data[:, :, 10] = remain == 2                     ## 10
+        data[:, :, 11] = remain == 3                     ## 11
+        data[:, :, 12] = remain == 4                     ## 12
         for pos in range(3):
-            for num in range(4):
-                data[pos, :, num] = (arena.remain[pos] > num)
-                if pos != arena.pos:
-                    data[pos, :, num+4] = (arena.lastplay[pos] > num)
-        data[arena.pos, :, 8] = 1
+            if remain[pos, 13] == 1 and remain[pos, 14] == 1:
+                data[pos, 13, 12] = 1
+
+        for pos in range(3):
+            for i in range(8):
+                if np.all(remain[pos, i:(i+5)] >= 1):
+                    data[pos, i:(i+5), 13] = 1           ## 13
+            for i in range(10):
+                if np.all(remain[pos, i:(i+3)] >= 2):
+                    data[pos, i:(i+3), 14] = 1           ## 14  
+            for i in range(11):
+                if np.all(remain[pos, i:(i+2)] >= 3):
+                    data[pos, i:(i+2), 15] = 1           ## 15
+
         data.shape = (1, NCARDGROUPS, CARD_DIM, NCHANNEL)
         return data
 
@@ -86,10 +125,10 @@ class BOT:
 
     def netchoose(self, choices):
         '''
-        choices: 列表, 每一项是长度15的数组
+        choices: 列表，每一项是长度15的数组
             所有合法出牌
         
-        返回值: 长度15的数组
+        返回值：长度15的数组
             出这一手的局面估值最高
         '''
         if np.random.random() < self.epsilon:
@@ -113,23 +152,23 @@ class BOT:
 
     def get_dizhu_win_probs(self, choices):
         '''
-        choices: 列表, 每一项是长度15的数组
+        choices: 列表，每一项是长度15的数组
             所有合法出牌
         
-        返回值: 和choices一样长的列表, 每一项是数字 (浮点, 0到1之间)
+        返回值：和choices一样长的列表，每一项是数字（浮点，0到1之间）
             对应每一种出牌后的局面估值
         '''
         xs = []
         ##循环每一种出牌
         for choice in choices: 
             # print("choice ", vec2str(choice))
-            cp = self.arena.copy()  ##复制当前状态, 后续操作在副本上进行, 避免破坏当前状态
+            cp = self.arena.copy()  ##复制当前状态，后续操作在副本上进行，避免破坏当前状态
             cp.update(choice)  ##出牌
             xs.append(self.getdata(cp))
         xs = np.concatenate(xs)
         return xs, self.model(xs).numpy()[:,0]
 
-    ## 调试用, 打印某个局面下所有的出牌选择及相应的估值
+    ## 调试用，打印某个局面下所有的出牌选择及相应的估值
     def showChoices(self, NUM=None):
         arena = self.arena
         choices = arena.getChoices()
