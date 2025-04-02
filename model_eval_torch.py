d23cdc77cd334ae01792e12e153603d3fb7be19b
tangcxx <58771897+tangcxx@users.noreply.github.com>
2025-04-02 11:38:33 +0800

    新增模型评估和训练模块，支持模型池随机抽取以生成训练数据

---
+++ model_eval_torch.py
@@ -0,0 +1,73 @@
+# 以 bot_zero 为基准，评估不同训练方法的表现
+# 评估地主农民三个模型分开的训练方法
+
+import multiprocessing as mp
+from datetime import datetime
+
+import numpy as np
+import torch
+import rules
+import arena as arn
+ARENA = arn.ARENA
+
+import bot_douzero
+bot_rivals = [bot_douzero.BOT(0), bot_douzero.BOT(0), bot_douzero.BOT(0)]
+
+# 参数
+# three: 
+nproc = 6
+nround = 1000
+maxid = 385400
+minid = 301000
+len_segment = 80
+model_freq = 100
+model_path = 'model_torch_ln'
+from bot_torch_ln import BOT, Model
+
+def model_eval_worker(id):
+    torch.set_num_threads(1)
+    models = [Model(), Model(), Model()]
+    cp = torch.load(f"{model_path}/cp{id}.pt")
+    for pos in range(3):
+        models[pos].load_state_dict(cp["models_state_dict"][pos])
+        models[pos].eval()
+    bots = [BOT(models, verbos=0), BOT(models, verbos=0), BOT(models, verbos=0)]
+
+    n_dizhu_win, n_farmer_win = 0, 0
+    for _ in range(nround):
+        cards = rules.CARDS.copy()
+        np.random.shuffle(cards)
+        
+        arena = ARENA(verbos=0, cards=cards.copy())
+        arena.registerbot([bots[0], bot_rivals[1], bot_rivals[2]])
+        arena.wholegame()
+        n_dizhu_win += (arena.winner == 0)
+        
+        arena2 = ARENA(verbos=0, cards=cards.copy())
+        arena2.registerbot([bot_rivals[0], bots[1], bots[2]])
+        arena2.wholegame()
+        n_farmer_win += (arena2.winner != 0)
+        
+        total_win = n_dizhu_win + n_farmer_win
+        win_percent = total_win / (nround * 2)
+    return id, n_dizhu_win, n_farmer_win, total_win, win_percent
+
+def model_eval(minid, maxid):
+    mp.set_start_method('spawn')
+
+    ids = np.arange(minid, maxid + model_freq, model_freq)
+    ids_segs = [ids[i:i + len_segment] for i in range(0, len(ids), len_segment)]
+
+    f = open('{}/eval.txt'.format(model_path), 'a', buffering=1)
+    with mp.Pool(nproc) as p:
+        for ids_seg in ids_segs:
+            res = p.map(model_eval_worker, ids_seg)
+            for r in res:
+                f.write("{}\t{}\t{}\t{}\t{}\n".format(*r))
+            print(datetime.now(), res[-1][0])
+    f.close()
+    return res
+
+if __name__ == '__main__':
+    res = model_eval(minid, maxid)
+
---
+++ trainer_torch_pool.py
@@ -0,0 +1,174 @@
+# 基于trainer_torch_ln, 使用模型池, 从模型池中随机抽取模型生成训练数据
+
+#%%
+import os
+import torch
+from torch import nn
+import numpy as np 
+import multiprocessing as mp
+from datetime import datetime
+
+## 模型评价
+import bot_douzero
+from rules import CARDS
+nround_eval = 200
+bots_rival = [bot_douzero.BOT(), bot_douzero.BOT(), bot_douzero.BOT()]
+
+# 训练参数
+from arena import ARENA
+from bot_torch_ln import BOT, Model
+
+modelpath = "model_torch_pool"
+iterstart=499000
+model_freq = 100
+
+pool_size = 200
+pool_id_start=499000
+
+nproc = 8
+nmatch_per_iter = 24
+batch_size = 32
+epsilonstep=0.999
+epsilonmin=0.01
+
+bce = nn.BCELoss()
+# loss = nn.BCEWithLogitsLoss
+
+model_subs = [Model(), Model(), Model()]
+
+def selfplay(args):
+    torch.set_num_threads(1)
+    ws_0, ws_1, ws_2, epsilon = args
+    wss = [ws_0, ws_1, ws_2]
+    bots = [[], [], []]
+    for pos in range(3):
+        model_subs[pos].load_state_dict(wss[pos])
+        bots[pos] = BOT(models=model_subs, epsilon=epsilon)
+    arena = ARENA(RECORD=False)
+    arena.registerbot(bots)
+    arena.wholegame()
+    y = 1 if arena.winner == 0 else 0
+
+    xs = [bot.xs for bot in bots]
+    ys = [[y] * len(bot.xs) for bot in bots]
+
+    return xs, ys
+
+
+def eval(args):
+    for pos in range(3):
+        model_subs[pos].load_state_dict(args[pos])
+        bots = [BOT(models=model_subs), BOT(models=model_subs), BOT(models=model_subs)]
+
+    n_dizhu_win, n_farmer_win = 0, 0
+
+    cards = CARDS.copy()
+    np.random.shuffle(cards)
+    
+    arena = ARENA(verbos=0, cards=cards.copy())
+    arena.registerbot([bots[0], bots_rival[1], bots_rival[2]])
+    arena.wholegame()
+    n_dizhu_win += (arena.winner == 0)
+    
+    arena2 = ARENA(verbos=0, cards=cards.copy())
+    arena2.registerbot([bots_rival[0], bots[1], bots[2]])
+    arena2.wholegame()
+    n_farmer_win += (arena2.winner != 0)
+
+    return n_dizhu_win, n_farmer_win
+
+
+def checkpoint_save(iter, models, optizimers):
+    torch.save(
+                    {
+                        "models_state_dict": (models[0].state_dict(), models[1].state_dict(), models[2].state_dict()),
+                        "optizimers_state_dict": (optizimers[0].state_dict(), optizimers[1].state_dict(), optizimers[2].state_dict())
+                    },
+                    f"{modelpath}/cp{iter}.pt"
+                )
+
+def checkpoint_load(iter, models, optimizers):
+    cp = torch.load(f"{modelpath}/cp{iter}.pt")
+    for pos in range(3):
+        models[pos].load_state_dict(cp["models_state_dict"][pos])
+        optimizers[pos].load_state_dict(cp["optizimers_state_dict"][pos])
+
+def train():
+    mp.set_start_method('spawn')
+    p = mp.Pool(nproc)
+    iter = iterstart
+    
+    
+    models = [Model(), Model(), Model()]
+    optimizers = [torch.optim.Adam(models[pos].parameters(), lr=1e-4) for pos in range(3)]
+    if iter == 0:
+        os.makedirs(f"{modelpath}", exist_ok=True)
+        checkpoint_save(iter, models, optimizers)
+    else:
+        checkpoint_load(iter, models, optimizers)
+    
+    model_pools = []
+    for pool_id in range(pool_id_start, iterstart + 100, 100)[-pool_size:]:
+        cp = torch.load(f"{modelpath}/cp{pool_id}.pt")
+        model_pools.append(cp["models_state_dict"])
+        del cp
+    
+    f_log = open("{}/log.txt".format(modelpath), "a", buffering=1)
+    f_eval = open("{}/eval.txt".format(modelpath), "a", buffering=1)
+    while True:
+        epsilon = max(epsilonstep ** iter, epsilonmin)
+
+        ids = np.random.randint(0, len(model_pools), size=3)
+        print(ids)
+        res = p.map(selfplay, [(model_pools[ids[0]][0], model_pools[ids[1]][1], model_pools[ids[2]][2], epsilon)] * nmatch_per_iter)
+
+        xss, yss = [[], [], []], [[], [], []]
+        for r in res:
+            xs, ys = r
+            for pos in range(3):
+                xss[pos].extend(xs[pos])
+                yss[pos].extend(ys[pos])
+
+        for xs, ys, model, optimizer in zip(xss, yss, models, optimizers):
+            model.train()
+            if len(ys) == 0:
+                continue
+            xs = torch.from_numpy(np.concatenate(xs)).float()
+            ys = torch.tensor(ys).reshape(-1,1).float()
+            indices = list(range(ys.shape[0]))
+            np.random.shuffle(indices)
+            indices_lists = [indices[i:i + batch_size] for i in range(0, len(indices), batch_size)]
+            for indices in indices_lists:
+                optimizer.zero_grad()
+                xs_batch = xs[indices]
+                ys_batch = ys[indices]
+                loss_factor = (len(xs_batch) / batch_size) ** 0.5
+                loss = bce(model(xs_batch), ys_batch) * loss_factor 
+                loss.backward()
+                optimizer.step()
+
+        print(datetime.now(), iter)
+        f_log.write(f"{datetime.now()}\t{iter}\n")
+        iter += 1
+        if iter % model_freq == 0:
+            checkpoint_save(iter, models, optimizers)
+            
+            model_pools.append((models[0].state_dict(), models[1].state_dict(), models[2].state_dict()))
+            model_pools = model_pools[-pool_size:]
+
+            wins = p.map(eval, [(models[0].state_dict(), models[1].state_dict(), models[2].state_dict())] * nround_eval)
+            wins = np.array(wins).sum(axis=0)
+            wins_total = wins.sum()
+            f_eval.write(f"{iter}\t{wins[0]}\t{wins[1]}\t{wins_total}\t{wins_total/(nround_eval*2)}\n")
+
+        if iter % 200 == 0:
+            p.close()
+            p.join()
+            p = mp.Pool(nproc)
+
+
+
+#%%
+if __name__ == '__main__':
+   train()
+
